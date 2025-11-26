818eabb91097853768cb93ee8307f97e40155319
redeck <466959@niuitmo.ru>
2025-11-27 00:24:54 +0300

    Копирование по кнопке

--- frontend/src/components/Pre.jsx
+++ frontend/src/components/Pre.jsx
@@ -1,10 +1,12 @@
-import React from "react";
+import React, { useState, useRef } from "react";
 import copy from "../img/copy.png";
 import copy2x from "../img/copy@2x.png";
 import copy3x from "../img/copy@3x.png";
 
 const Pre = ({ children, ...props }) => {
     let language = "";
+    const [copied, setCopied] = useState(false);
+    const preRef = useRef(null);
 
     if (React.isValidElement(children)) {
         const className = children.props.className || "";
@@ -14,21 +16,44 @@ const Pre = ({ children, ...props }) => {
         }
     }
 
+    async function copyToClipboard(text) {
+        try {
+            await navigator.clipboard.writeText(text);
+            return true;
+        } catch (err) {
+            return false;
+        }
+    }
+
+    const handleCopy = async () => {
+        if (preRef.current) {
+            const codeText = preRef.current.innerText;
+            const success = await copyToClipboard(codeText);
+            if (success) {
+                setCopied(true);
+                setTimeout(() => setCopied(false), 2000);
+            }
+        }
+    };
+
     return (
         <div className="code-container">
             <div className="code-header">
                 <span>{language}</span>
-                <button className="Copy-Button">
+                <button className="Copy-Button" onClick={handleCopy}>
                     <img
                         src={copy}
                         className="copy"
                         srcSet={`${copy2x}, ${copy3x}`}
-                    />{" "}
-                    Copy
+                        alt="copy icon"
+                    />
+                    {copied ? "Copied" : "Copy"}
                 </button>
             </div>
 
-            <pre {...props}>{children}</pre>
+            <pre ref={preRef} {...props}>
+                {children}
+            </pre>
         </div>
     );
 };
